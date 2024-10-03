0091a34c4003fc5f70ea8380e5522262b41b57d2
KishoreKode <kishoreganth700@gmail.com>
2024-10-03 14:38:59 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env:{
+         url:'https://4c9a-2401-4900-629b-d158-c97c-af5d-7b52-8487.ngrok-free.app/',
+         boturl:'https://kayzen-backend.onrender.com'
+    }
+};
+
+export default nextConfig;
